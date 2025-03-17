49b8ee848c543418f7942c238157a5214bf0583e
dgsim <dgsim126@gmail.com>
2025-03-17 23:45:53 +0900

    first commit

---
+++ src/main/java/com/donggeun/githubcationsbasic/GithubCationsBasicApplication.java
@@ -0,0 +1,13 @@
+package com.donggeun.githubcationsbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCationsBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCationsBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donggeun/githubcationsbasic/GithubCationsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggeun.githubcationsbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCationsBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
