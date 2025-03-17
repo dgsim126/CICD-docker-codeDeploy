99017c57fc8810fd204836d93550ee7fbe65f880
dgsim <dgsim126@gmail.com>
2025-03-18 00:54:50 +0900

    project setting

---
+++ src/main/java/com/donggeun/githubcationsbasic/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.donggeun.githubcationsbasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+}
