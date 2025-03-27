b15042cdfbec007c1e43ec0918b5983b11069b32
donggeun <dgsim126@gmail.com>
2025-03-27 18:40:06 +0900

    final

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/instagram-server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop instagram-server || true
+docker rm instagram-server || true
+docker pull 810456925233.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server/instagram-server:latest
+docker run -d --name instagram-server -p 8080:8080 810456925233.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server/instagram-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
